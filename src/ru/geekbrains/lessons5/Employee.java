e686a60dd69e9af6c06f111449bb365ba3b4cd30
bergik37 <bergik@yandex.ru>
2020-12-30 20:07:48 +0300

    Lesson5

--- src/ru/geekbrains/lessons5/Employee.java
+++ src/ru/geekbrains/lessons5/Employee.java
@@ -1,6 +1,6 @@
 package ru.geekbrains.lessons5;
 
-public class Employee {
+public class Employee{
     private String fio;
     private String post;
     private String email;
@@ -18,50 +18,43 @@ public class Employee {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public int getTelephoneNumber() {
         return telephoneNumber;
     }
-
     public void setTelephoneNumber(int telephoneNumber) {
         this.telephoneNumber = telephoneNumber;
     }
-
     public int getSalary() {
         return salary;
     }
-
     public void setSalary(int salary) {
         this.salary = salary;
     }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public void setFio(String fio){
-    }
-    public String getFio(){
-        return fio;
-    }
-    public void setPost(String post){
-    }
-    public String getPost(){
-        return post;
-    }
+    public int getAge() {return age;}
+    public void setAge(int age) { }
+    public void setFio(String fio){ }
+    public String getFio(){ return fio; }
+    public void setPost(String post){ }
+    public String getPost(){ return post; }
 
-    public void info(){
-        System.out.println("ФИО "+fio+"; должность "+post+"; Email "+email+"; Номер телефона "+telephoneNumber+"; Зарплата "+post+"у.е."+"; возраст "+age);
+    public void info() {
+        if (getAge() >= 40) {
+            System.out.println("ФИО " + getFio() + "; должность " + getPost() + "; Email " + getEmail() + "; Номер телефона " + getTelephoneNumber() + "; Зарплата " + getSalary() + "у.е." + "; возраст " + getAge());
+        }
     }
 
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
         employees[0]= new Employee("Ivanov Ivan","Mechanic","IvanovIvan@gamil.ru",896523123,21000,26);
+        employees[1]= new Employee("Ivanov1 Ivan1","Mechanic","IvanovIvan@gamil.ru",896523123,17000,19);
+        employees[2]= new Employee("Ivanov1 Ivan1","Mechanic","IvanovIvan@gamil.ru",896523123,24000,23);
+        employees[3]= new Employee("Ivanov1 Ivan1","Mechanic","IvanovIvan@gamil.ru",896523123,38000,42);
+        employees[4]= new Employee("Ivanov1 Ivan1","Mechanic","IvanovIvan@gamil.ru",896523123,41000,45);
+        for (int i = 0;i <employees.length; i++) {
+            employees[i].info();
+        }
     }
 }
-
